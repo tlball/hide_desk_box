2fc8787590534b58d1cf65c7847f572b9139b563
Tricia Ball <tball@tball.local>
2015-02-27 15:20:00 -0600

    Renamed script

--- myScript.js
+++ myScript.js
@@ -1,1 +1,9 @@
-$('textarea.reply_area').hide();
+//  alert(document.querySelector('[id^="email_reply_area_ticket_"]').id);
+//document.getElementsByClassName('reply_area')[0].id;
+//function removeBox() {
+//  alert(document.body.innerHTML);
+//};
+//window.onload="removeBox()";
+if (document.querySelector('[id^="email_reply_area_ticket_"]') !== undefined ) {
+function(){document.querySelector('[id^="email_reply_area_ticket_"]').style.display='none';}
+}
