768196f6277bc2220245dea7396c5e1307b3b77c
Tricia Ball <tball@tball.local>
2015-02-27 15:21:26 -0600

    Brought in jQuery (likely overkill) and refactored to pull out js into its own function

--- myScript.js
+++ myScript.js
@@ -1,9 +1,12 @@
-//  alert(document.querySelector('[id^="email_reply_area_ticket_"]').id);
-//document.getElementsByClassName('reply_area')[0].id;
-//function removeBox() {
-//  alert(document.body.innerHTML);
-//};
-//window.onload="removeBox()";
-if (document.querySelector('[id^="email_reply_area_ticket_"]') !== undefined ) {
-function(){document.querySelector('[id^="email_reply_area_ticket_"]').style.display='none';}
+$(document).ready( function () {
+  hideClientResponseBox();
+});
+
+function hideClientResponseBox() {
+  try {
+// Turn off the extension if you want to write a message
+    document.querySelector('[id^="email_reply_area_ticket_"]').style.display='none';
+  } catch (e) {
+    //no op
+  }
 }
