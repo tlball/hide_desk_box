4814849ce6beff224f1e555edb777bb2cce50883
Tricia Ball <tball@tball.local>
2015-04-21 14:57:09 -0500

    (m) remove console log output

--- myScript.js
+++ myScript.js
@@ -11,7 +11,6 @@ function hideClientResponseBox() {
 // This is really ugly and not optimized, but works :) - hits 100+ times
 document.addEventListener('DOMNodeInserted', function (event) {
   if($(event.target).find('[id^="email_reply_area_ticket_"]').length > 0){
-    console.log('I made it!');
     hideClientResponseBox();
   }
 });
