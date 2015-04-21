4f74ebe7b58992221255641b118c26b11aa92f95
Tricia Ball <tball@tball.local>
2015-04-21 16:27:12 -0500

    Change to disable textarea with placeholder text instead of hiding it.

--- myScript.js
+++ myScript.js
@@ -1,7 +1,7 @@
-function hideClientResponseBox() {
+function disableClientResponseBox() {
   try {
-// Turn off the extension if you want to write a message
-    document.querySelector('[id^="email_reply_area_ticket_"]').style.display='none';
+    $('[id^="email_reply_area_ticket_"]').attr("disable", "disabled");
+    $('[id^="email_reply_area_ticket_"]').attr("placeholder", "Disabled to prevent unexpected emails to customers. \nIf you want to email customer, disable the Hide Desk Box extension.");
   } catch (e) {
     //no op
     console.log(e);
@@ -11,6 +11,6 @@ function hideClientResponseBox() {
 // This is really ugly and not optimized, but works :) - hits 100+ times
 document.addEventListener('DOMNodeInserted', function (event) {
   if($(event.target).find('[id^="email_reply_area_ticket_"]').length > 0){
-    hideClientResponseBox();
+    disableClientResponseBox();
   }
 });
