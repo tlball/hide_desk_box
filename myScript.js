1a45408de69b264f011640a4c018a49c42d239a4
Tricia Ball <tball@tball.local>
2015-04-30 16:41:42 -0500

    Corrected 'disabled' attribute

--- myScript.js
+++ myScript.js
@@ -1,6 +1,6 @@
 function disableClientResponseBox() {
   try {
-    $('[id^="email_reply_area_ticket_"]').attr("disable", "disabled");
+    $('[id^="email_reply_area_ticket_"]').attr("disabled", "disabled");
     $('[id^="email_reply_area_ticket_"]').attr("placeholder", "Disabled to prevent unexpected emails to customers. \nIf you want to email customer, disable the Continuity Desk Tweaks extension.");
   } catch (e) {
     //no op
